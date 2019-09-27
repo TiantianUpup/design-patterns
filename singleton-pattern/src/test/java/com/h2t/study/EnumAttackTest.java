206494e8f3c9a904f015e04abc8bd596af52057a
hetiantian <969795191@qq.com>
2019-09-27 10:28:05 +0800

    add enum attack test

---
+++ singleton-pattern/src/test/java/com/h2t/study/EnumAttackTest.java
@@ -0,0 +1,29 @@
+package com.h2t.study;
+
+import org.apache.commons.lang3.SerializationUtils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * 枚举创建单例对象攻击测试
+ *
+ * @author hetiantian
+ * @version 1.0
+ * @Date 2019/09/27 10:18
+ */
+public class EnumAttackTest {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
+        EnumSingleton enumSingleton = EnumSingleton.getEnumSingleton();
+        //序列化攻击
+        byte[] serialize4 = SerializationUtils.serialize(enumSingleton);
+        EnumSingleton enumSingleton2 = SerializationUtils.deserialize(serialize4);
+        System.out.println(enumSingleton == enumSingleton2);
+
+        //反射攻击
+        Constructor<EnumSingleton> enumSingletonConstructor = EnumSingleton.class.getConstructor();
+        enumSingletonConstructor.setAccessible(true);
+        EnumSingleton enumSingleton1 = enumSingletonConstructor.newInstance();
+        System.out.println(enumSingleton == enumSingleton1);
+    }
+}
